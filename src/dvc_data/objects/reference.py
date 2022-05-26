321a4ad19aeff99e10eadf7763d6ab3015952281
Ruslan Kuprieiev <kupruser@gmail.com>
2022-05-26 11:24:47 +0300

    ref: separate raw objects from dereferencing
    Fixes #29

--- src/dvc_data/objects/reference.py
+++ src/dvc_data/objects/reference.py
@@ -1,19 +1,25 @@
-import errno
 import logging
 import os
 import pickle
-from typing import TYPE_CHECKING, Optional
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 from dvc_objects.errors import ObjectFormatError
 from dvc_objects.file import HashFile
 from dvc_objects.fs import FS_MAP, LocalFileSystem
 
 if TYPE_CHECKING:
-    from dvc_objects.db import ObjectDB
     from dvc_objects.fs.base import AnyFSPath, FileSystem
     from dvc_objects.hash_info import HashInfo
 
 logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Reference:
+    fs_path: "AnyFSPath"
+    fs: "FileSystem"
+    checksum: int
 
 
 class ReferenceHashFile(HashFile):
@@ -28,61 +34,50 @@ class ReferenceHashFile(HashFile):
         fs_path: "AnyFSPath",
         fs: "FileSystem",
         hash_info: "HashInfo",
-        checksum: Optional[str] = None,
-        **kwargs,
+        ref: "Reference",
     ):
-        super().__init__(fs_path, fs, hash_info, **kwargs)
-        self.checksum = checksum or fs.checksum(fs_path)
+        super().__init__(fs_path, fs, hash_info)
+        self.ref = ref
 
-    def __str__(self):
-        return f"ref object {self.hash_info} -> {self.fs_path}"
+    def deref(self):
+        return HashFile(self.ref.fs_path, self.ref.fs, self.hash_info)
 
-    def check(self, odb: "ObjectDB", check_hash: bool = True):
-        assert self.fs
-        if not self.fs.exists(self.fs_path):
-            raise FileNotFoundError(
-                errno.ENOENT, os.strerror(errno.ENOENT), self.fs_path
-            )
-        if self.checksum != self._get_checksum():
-            raise ObjectFormatError(f"{self} is changed")
-        if check_hash:
-            self._check_hash(odb)
+    @staticmethod
+    def config_tuple(fs: "FileSystem"):
+        return (
+            fs.protocol,
+            tuple(
+                (key, value)
+                for key, value in sorted(
+                    fs.config.items(), key=lambda item: item[0]
+                )
+            ),
+        )
 
-    def _get_checksum(self) -> str:
-        assert self.fs
-        return self.fs.checksum(self.fs_path)
-
-    def to_bytes(self):
+    def as_dict(self):
         # NOTE: dumping reference FS's this way is insecure, as the
         # fully parsed remote FS config will include credentials
         #
         # ReferenceHashFiles should currently only be serialized in
         # memory and not to disk
-        fs_path = self.fs_path
-        fs_cls = type(self.fs)
+        fs_cls = type(self.ref.fs)
         mod = None
         if fs_cls not in FS_MAP.values() and fs_cls != LocalFileSystem:
             mod = ".".join((fs_cls.__module__, fs_cls.__name__))
-        dict_ = {
-            self.PARAM_PATH: fs_path,
+
+        fs_config = self.config_tuple(self.ref.fs)
+
+        return {
+            self.PARAM_PATH: self.ref.fs_path,
             self.PARAM_HASH: self.hash_info,
-            self.PARAM_CHECKSUM: self.checksum,
-            self.PARAM_FS_CONFIG: self.config_tuple(self.fs),
+            self.PARAM_CHECKSUM: self.ref.checksum,
+            self.PARAM_FS_CONFIG: fs_config,
             self.PARAM_FS_CLS: mod,
         }
-        try:
-            return pickle.dumps(dict_)
-        except pickle.PickleError as exc:
-            raise ObjectFormatError(f"Could not pickle {self}") from exc
 
     @classmethod
-    def from_bytes(cls, data: bytes, fs_cache: Optional[dict] = None):
+    def from_dict(cls, dict_, fs_cache=None):
         from dvc_objects.fs import get_fs_cls
-
-        try:
-            dict_ = pickle.loads(data)
-        except pickle.PickleError as exc:
-            raise ObjectFormatError("ReferenceHashFile is corrupted") from exc
 
         try:
             fs_path = dict_[cls.PARAM_PATH]
@@ -97,21 +92,75 @@ class ReferenceHashFile(HashFile):
             mod = dict_.get(cls.PARAM_FS_CLS, None)
             fs_cls = get_fs_cls(config, cls=mod, scheme=protocol)
             fs = fs_cls(**config)
-        return ReferenceHashFile(
-            fs_path,
-            fs,
-            hash_info,
-            checksum=dict_.get(cls.PARAM_CHECKSUM),
-        )
 
-    @staticmethod
-    def config_tuple(fs: "FileSystem"):
-        return (
-            fs.protocol,
-            tuple(
-                (key, value)
-                for key, value in sorted(
-                    fs.config.items(), key=lambda item: item[0]
-                )
-            ),
-        )
+        checksum = dict_.get(cls.PARAM_CHECKSUM)
+        hash_info = dict_.get(cls.PARAM_HASH)
+
+        ref = Reference(fs_path, fs, checksum)
+
+        return cls(None, None, hash_info, ref)
+
+    def as_bytes(self):
+        return pickle.dumps(self.as_dict())
+
+    @classmethod
+    def from_bytes(cls, byts, **kwargs):
+        try:
+            data = pickle.loads(byts)
+        except pickle.PickleError as exc:
+            raise ObjectFormatError("ReferenceHashFile is corrupted") from exc
+
+        return cls.from_dict(data, **kwargs)
+
+    @classmethod
+    def from_raw(cls, raw, **kwargs):
+        byts = raw.fs.cat_file(raw.fs_path)
+
+        obj = cls.from_bytes(byts, **kwargs)
+        obj.fs_path = raw.fs_path
+        obj.fs = raw.fs
+
+        if raw.hash_info != obj.hash_info:
+            raise ObjectFormatError(
+                "ReferenceHashFile is corrupted: hash mismatch"
+            )
+
+        return obj
+
+    @classmethod
+    def from_path(cls, fs_path, fs, hash_info, fs_cache=None):
+        from dvc_objects.fs import MemoryFileSystem, Schemes
+        from dvc_objects.fs.utils import tmp_fname
+
+        checksum = fs.checksum(fs_path)
+        ref = Reference(fs_path, fs, checksum)
+
+        if fs_cache and fs.protocol != Schemes.LOCAL:
+            fs_cache[cls.config_tuple(fs)] = fs
+
+        memfs = MemoryFileSystem()
+        memfs_path = "memory://{}".format(tmp_fname(""))
+
+        obj = cls(memfs_path, memfs, hash_info, ref)
+
+        try:
+            obj.fs.pipe_file(obj.fs_path, obj.as_bytes())
+        except OSError as exc:
+            if isinstance(exc, FileExistsError) or (
+                os.name == "nt"
+                and exc.__context__
+                and isinstance(exc.__context__, FileExistsError)
+            ):
+                logger.debug("'%s' file already exists, skipping", obj.fs_path)
+            else:
+                raise
+
+        return obj
+
+    def check(self, *args, **kwargs):
+        if not self.fs.exists(self.fs_path):
+            raise FileNotFoundError
+
+        actual = self.ref.fs.checksum(self.ref.fs_path)
+        if self.ref.checksum != actual:
+            raise ObjectFormatError(f"{self} is changed")
