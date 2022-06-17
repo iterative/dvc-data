8fe3311d4b9c8661838adc1c7fada86303c62b24
Ruslan Kuprieiev <kupruser@gmail.com>
2022-06-17 21:16:23 +0300

    ref: fix hash

--- src/dvc_data/db/reference.py
+++ src/dvc_data/db/reference.py
@@ -16,6 +16,9 @@ class ReferenceHashFileDB(HashFileDB):
         super().__init__(fs, path, **config)
         self._obj_cache: Dict["str", "HashFile"] = {}
 
+    def __hash__(self):
+        return hash((self.fs.protocol, self.path, *self._obj_cache.keys()))
+
     def exists(self, oid: str) -> bool:
         return oid in self._obj_cache
 
