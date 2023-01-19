173a87c3a8a4bda539f682f77ab2e68b67ff9a74
Peter Rowlands <peter@pmrowla.com>
2023-01-19 17:04:58 +0900

    index: use batched generic.transfer on checkout

--- src/dvc_data/index/checkout.py
+++ src/dvc_data/index/checkout.py
@@ -1,18 +1,17 @@
-from functools import partial
+from collections import defaultdict
 from typing import (
     TYPE_CHECKING,
     Collection,
     Dict,
     Iterator,
     List,
-    MutableSequence,
     Optional,
     Tuple,
 )
 
-from dvc_objects.executors import ThreadPoolExecutor
 from dvc_objects.fs.callbacks import DEFAULT_CALLBACK, Callback
 from dvc_objects.fs.generic import transfer
+from dvc_objects.fs.utils import exists as batch_exists
 
 from ..hashfile.meta import Meta
 from .diff import ADD, DELETE, MODIFY, diff
@@ -57,51 +56,55 @@ def checkout(
                 _prune_existing_versions(create, fs, path, callback=cb)
             )
 
-    if callback != DEFAULT_CALLBACK:
-        callback.set_size(
-            sum(
-                entry.meta is not None and not entry.meta.isdir
-                for _, entry in index.iteritems()
-            )
+    fs_map: Dict[
+        "FileSystem", List[Tuple["DataIndexEntry", str, str]]
+    ] = defaultdict(list)
+    parents = set()
+    for entry in create:
+        if entry.meta and entry.meta.isdir:
+            continue
+        dest_path = fs.path.join(path, *entry.key)
+        parents.add(fs.path.parent(dest_path))
+        if entry.fs and entry.path:
+            src_fs: "FileSystem" = entry.fs
+            src_path = entry.path
+        else:
+            assert entry.hash_info
+            odb = entry.odb or entry.cache or entry.remote
+            assert odb
+            src_fs = odb.fs
+            src_path = odb.oid_to_path(entry.hash_info.value)
+        fs_map[src_fs].append((entry, src_path, dest_path))
+
+    for parent in parents:
+        fs.makedirs(parent, exist_ok=True)
+    for src_fs, args in fs_map.items():
+        entries, src_paths, dest_paths = zip(*args)
+        transfer(
+            src_fs,
+            list(src_paths),
+            fs,
+            list(dest_paths),
+            callback=callback,
+            batch_size=jobs,
         )
-    processor = partial(
-        _do_create,
-        path,
-        fs,
-        callback=callback,
-        update_meta=update_meta,
-    )
-    with ThreadPoolExecutor(max_workers=jobs or fs.jobs) as executor:
-        transferred += sum(executor.imap_unordered(processor, create))
+        transferred += len(entries)
+        if update_meta:
+            if callback == DEFAULT_CALLBACK:
+                cb = callback
+            else:
+                desc = f"Updating meta for new files in '{path}'"
+                cb = Callback.as_tqdm_callback(desc=desc, unit="file")
+            with cb:
+                cb.set_size(len(args))
+                for entry, _src_path, dest_path in args:
+                    entry.fs = fs
+                    entry.path = dest_path
+                    entry.meta = Meta.from_info(
+                        fs.info(dest_path), fs.protocol
+                    )
+                    cb.relative_update()
     return transferred
-
-
-def _do_create(
-    path: str,
-    fs: "FileSystem",
-    entry: "DataIndexEntry",
-    callback: "Callback" = DEFAULT_CALLBACK,
-    update_meta: bool = True,
-) -> int:
-    assert entry.meta
-    if entry.meta.isdir:
-        return 0
-
-    entry_path = fs.path.join(path, *entry.key)
-    sources = []
-    if entry.hash_info:
-        odb = entry.odb or entry.cache or entry.remote
-        assert odb
-        sources.append((odb.fs, odb.oid_to_path(entry.hash_info.value)))
-    if entry.fs and entry.path:
-        sources.append((entry.fs, entry.path))
-    fs.makedirs(fs.path.parent(entry_path), exist_ok=True)
-    _try_sources(fs, entry_path, sources, callback=callback)
-    if update_meta:
-        entry.fs = fs
-        entry.path = entry_path
-        entry.meta = Meta.from_info(fs.info(entry_path), fs.protocol)
-    return 1
 
 
 def _get_changes(
@@ -120,22 +123,6 @@ def _get_changes(
     return create, delete
 
 
-def _try_sources(
-    fs: "FileSystem",
-    entry_path: str,
-    sources: MutableSequence[Tuple["FileSystem", str]],
-    **kwargs,
-):
-    while sources:
-        src_fs, src_path = sources.pop(0)
-        try:
-            transfer(src_fs, src_path, fs, entry_path, **kwargs)
-            return
-        except Exception:  # pylint: disable=broad-except
-            if not sources:
-                raise
-
-
 def _prune_existing_versions(
     entries: Collection["DataIndexEntry"],
     fs: "FileSystem",
@@ -143,8 +130,6 @@ def _prune_existing_versions(
     callback: "Callback" = DEFAULT_CALLBACK,
     jobs: Optional[int] = None,
 ) -> Iterator["DataIndexEntry"]:
-    from dvc_objects.fs.utils import exists as batch_exists
-
     assert fs.version_aware
     query_vers: Dict[str, "DataIndexEntry"] = {}
     jobs = jobs or fs.jobs
