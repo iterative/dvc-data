c25b608ee390418dbdee1c159150d4e685973554
skshetry <18718008+skshetry@users.noreply.github.com>
2024-02-28 18:34:12 +0545

    use functools.cached_property on >=3.12 (#511)
    - Also removes shortuuid, and uses `tmp_fname` from `dvc_objects`.
    - After removing funcy.cached_property, the only use of
      funcy was for `split`, which has been refactored as well.
      So, we also don't need to depend on funcy now.

--- src/dvc_data/compat.py
+++ src/dvc_data/compat.py
@@ -1,6 +1,7 @@
+import sys
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
+if sys.version_info >= (3, 12) or TYPE_CHECKING:
     from functools import cached_property  # noqa: TID251
 else:
     from funcy import cached_property  # noqa: TID251
