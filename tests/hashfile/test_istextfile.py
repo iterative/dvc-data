c1abe16913b04f23160d86194218b1068b51e04c
skshetry <18718008+skshetry@users.noreply.github.com>
2025-01-14 14:38:59 +0545

    cleanup cli and tests (#593)

--- tests/hashfile/test_istextfile.py
+++ tests/hashfile/test_istextfile.py
@@ -1,6 +1,5 @@
-import sys
-
 import pytest
+from dvc_objects.fs.memory import MemoryFileSystem
 
 from dvc_data.hashfile.istextfile import istextblock, istextfile
 
@@ -26,10 +25,8 @@ def test_istextblock(block, expected):
     assert istextblock(block) is expected
 
 
-@pytest.mark.parametrize("tmp_upath", ["local", "s3"], indirect=True)
-def test_istextfile(tmp_upath, block, expected):
-    if sys.version_info >= (3, 13) and "s3" in tmp_upath.fs.protocol:
-        pytest.skip("universal-pathlib does not support Python 3.13 yet.")
-    path = tmp_upath / "file"
-    path.write_bytes(block)
-    assert istextfile(path.path, path.fs) is expected
+def test_istextfile(block, expected):
+    fs = MemoryFileSystem(global_store=False)
+    fs.pipe_file("/file", block)
+
+    assert istextfile("/file", fs) is expected
